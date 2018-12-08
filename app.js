d9608a66202a2e315f93685914f796f27e0b2642
Charidimos Tzedakis <harris.jedakis@gmail.com>
2018-12-09 00:34:27 +0200

    work on algorithm

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const datapoints = require('./datapoints');
 
-function sortByStart(a,b) {
-    if (a.start < b.start)
+function sortByTierLevel(a,b) {
+    if (a.tier < b.tier)
       return -1;
-    if (a.start > b.start)
+    if (a.tier > b.tier)
       return 1;
     return 0;
 }
@@ -11,24 +11,39 @@ function sortByStart(a,b) {
 function createDataTree (datapoints) {
 
     //* 1. sort based on tier 
-    const tierSorted = datapoints.sort(sortByStart);
+    const tierSorted = datapoints.sort(sortByTierLevel);
     
     //* create tree structure
     const dataTree = [];
     let currentParentNode = null;
     let parentNodeStack = []; //* this stack holds data nodes that are not yet finalized
     let currentNode = null;
+    let dataTree = [];
     for (var datapoint of tierSorted) {
       currentNode = Object.assign(datapoint, {children:[]});
-      if (!currentParentNode) {
+      if (!currentParentNode) { //* top level node
         currentParentNode = currentNode;
+        dataTree.push(currentParentNode);
       } else {
-        if (currentNode.tier === currentParentNode.tier ) { //* currentNode: will be child of current parent and wont have children
+        if (currentNode.tier === currentParentNode.tier ) { //* currentNode: is a leaf of the tree
           currentParentNode.children.push(currentNode);
         } else if (currentNode.tier.startsWith(currentParentNode.tier)) { //* currentNode: will be child of current parent and might have children
           currentParentNode.children.push(currentNode);
           parentNodeStack.push(currentParentNode); //* push currentParentNode to the stack
           currentParentNode = currentNode; //* this node is now the current parent
+        } else {
+          currentParentNode = parentNodeStack.pop();
+          while (currentParentNode && !currentNode.tier.startsWith(currentParentNode.tier)) {
+            currentParentNode = parentNodeStack.pop();
+          }
+          if (!currentParentNode) {
+            dataTree.push(currentNode); //* fount a top level node
+          }
+          else {  //* case: currentNode.tier.startsWith(currentParentNode.tier)
+            currentParentNode.children.push(currentNode);
+            parentNodeStack.push(currentParentNode);
+            currentParentNode = currentNode; 
+          }
         }
       }
     }
